e19f00a81061df892abac30f378359e7dce28125
Louis Vanier <louis.vanier@shopify.com>
2018-11-30 14:50:19 -0500

    add facebook, better looking sign in buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: :login
   def login
+    render :login, layout: 'layouts/login'
   end
 
   def index
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -289,4 +289,5 @@ Devise.setup do |config|
   # end
 
   config.omniauth :google_oauth2, Rails.application.credentials.google_oauth2[:id], Rails.application.credentials.google_oauth2[:secret], {}
+  config.omniauth :facebook,  Rails.application.credentials.facebook[:id], Rails.application.credentials.facebook[:key]
 end
