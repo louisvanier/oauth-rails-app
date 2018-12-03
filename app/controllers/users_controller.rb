c75e85043e6aceb40ef976158b74f019c06a218d
Louis Vanier <vanier.louis@gmail.com>
2018-12-02 21:31:29 -0500

    log session content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: :login
   def login
-    @user = User.find_by(id: session[:user_id]) if session[:user_id]
+    Rails.logger.info("#{session}")
+    @user = current_user
     render :login, layout: 'layouts/login'
   end
 
