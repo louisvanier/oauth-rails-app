f72ac8a8f24670bbce2f73e341923282aaee107f
Louis Vanier <vanier.louis@gmail.com>
2018-12-02 21:14:32 -0500

    load already logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: :login
   def login
-  	@user = current_user
+    @user = User.find_by(id: session[:user_id]) if session[:user_id]
     render :login, layout: 'layouts/login'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   root 'users#login'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', as: :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
