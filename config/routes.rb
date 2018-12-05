d2be97d5d7108ee05ca142cb9e4345f9aa1cba2b
Louis Vanier <vanier.louis@gmail.com>
2018-12-05 17:15:18 -0500

    fix user login

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
-    get 'sign_in', :to => 'devise/sessions#new', as: :new_user_session
+    get 'sign_in', :to => 'users#login', as: :new_user_session
+    post 'sign_in', to: 'devise/sessions#create', as: :user_session
     get 'sign_out', :to => 'devise/sessions#destroy', as: :destroy_user_session
   end
 end
