3b64fd4927d9d4a404d9eedbcb25a3fdd3ab66e4
Louis Vanier <vanier.louis@gmail.com>
2018-12-05 19:06:37 -0500

    add admin status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_tenant
 
   def is_public_apartment?
     Apartment::Tenant.current == 'public'
