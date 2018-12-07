cf7d3cacf3b7fe7a7f3bf646939f63673d39d291
Louis Vanier <vanier.louis@gmail.com>
2018-12-07 12:55:42 -0500

    refactor oauth callbacks to remove duplication

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,28 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in @user, event: :authentication
-      redirect_to get_redirection_path(@user.email)
-    else
-      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    end
+    handle_callback(provider_name: 'google')
   end
 
   def facebook
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook' if is_navigational_format?
-      sign_in @user, event: :authentication
-      redirect_to get_redirection_path(@user.email)
-    else
-      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    end
+    handle_callback(provider_name: 'facebook')
   end
 
   def failure
@@ -32,11 +14,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   private
 
-  def get_redirection_path(user)
-    if current_tenant.is_admin?(user)
+  def get_redirection_path(email_address)
+    if current_tenant.is_admin?(email_address)
       users_index_path
     else
       revenue_shares_new_path
+    end
+  end
+
+  def handle_callback(provider_name:)
+    @user = User.from_omniauth(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      flash[:notice] = 'Your authentication was successful, an administrator will approve your account' unless @user.approved
+      sign_in @user, event: :authentication
+      redirect_to get_redirection_path(@user.email)
+    else
+      session["devise.#{provider_name}_data"] = request.env['omniauth.auth'].except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
 end
