7d6d0502a81ba18d050f079cf9c9da178f983083
Louis Vanier <vanier.louis@gmail.com>
2018-12-16 21:35:44 -0500

    fix local variable issue on action mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
     Apartment::Tenant.switch(tenant.subdomain) do
       @new_user = User.find_by(id: params[:user_id])
       @user_index_url = params[:user_index_url]
-      mail(to: tenant.admins.join(','), subject: "[#{tenant.subdomain}] A new user has registered: #{new_user.name}")
+      mail(to: tenant.admins.join(','), subject: "[#{tenant.subdomain}] A new user has registered: #{@new_user.name}")
     end
   end
 end
