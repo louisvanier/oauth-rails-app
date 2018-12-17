9cc2dc3ecf4e126a2e0da047f75b5509e2033dbe
Louis Vanier <vanier.louis@gmail.com>
2018-12-16 21:28:33 -0500

    fix activejob deserialization issue with Apartment

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -41,6 +41,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def mail_new_user(user)
-    UserMailer.with(tenant: current_tenant, user: user, user_index_url: users_manage_url).new_user_created.deliver_later
+    UserMailer.with(tenant: current_tenant, user_id: user.id, user_index_url: users_manage_url).new_user_created.deliver_later
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,10 @@ class UserMailer < ApplicationMailer
 
   def new_user_created
     tenant = params[:tenant]
-    @new_user = params[:user]
-    @user_index_url = params[:user_index_url]
-    mail(to: tenant.admins.join(','), subject: "[#{tenant.subdomain}] A new user has registered: #{new_user.name}")
+    Apartment::Tenant.switch(tenant.subdomain) do
+      @new_user = User.find_by(id: params[:user_id])
+      @user_index_url = params[:user_index_url]
+      mail(to: tenant.admins.join(','), subject: "[#{tenant.subdomain}] A new user has registered: #{new_user.name}")
+    end
   end
 end
