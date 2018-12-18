97028eed834fe1f06517e7e5fc153a0e679f52cd
Louis Vanier <vanier.louis@gmail.com>
2018-12-18 14:44:29 -0500

    dynamic manifest.json

--- app/controllers/pwa_controller.rb
+++ app/controllers/pwa_controller.rb
@@ -1,7 +1,5 @@
 class PwaController < ApplicationController
   def manifest
-    respond_to do |format|
-      format.json render partial: 'pwa/manifest.json'
-    end
+    render partial: 'pwa/manifest.json'
   end
 end
